69068f3a947b22ba5f3887e7e512009cbe1c6481
VladlmlrF <vladimirfalin84@gmail.com>
2023-06-25 21:44:06 +0700

    add post_detail page

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -8,3 +8,9 @@ posts = Blueprint('posts', __name__, template_folder='templates')
 def index_page():
     psts = Post.query.all()
     return render_template('index.html', posts=psts)
+
+
+@posts.route('/<name>')
+def post_detail_page(name):
+    post = Post.query.filter(Post.name == name).first()
+    return render_template('post_detail.html', post=post)
