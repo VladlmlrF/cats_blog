c79bcb2d4bda97478c0540d19522a5d180c2d677
VladlmlrF <vladimirfalin84@gmail.com>
2023-06-29 14:35:19 +0700

    create search

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -7,29 +7,6 @@ from .forms import PostForm
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
-@posts.route('/')
-def index_page():
-    psts = Post.query.all()
-    return render_template('index.html', posts=psts)
-
-
-@posts.route('/<url>')
-def post_detail_page(url):
-    post = Post.query.filter(Post.url == url).first()
-    tags = post.tags
-    if post:
-        return render_template('post_detail.html', post=post, tags=tags)
-    else:
-        return render_template('page404.html'), 404
-
-
-@posts.route('/tag/<url>')
-def tag_detail_page(url):
-    tag = Tag.query.filter(Tag.url == url).first()
-    psts = tag.posts.all()
-    return render_template('tag_detail.html', tag=tag, psts=psts)
-
-
 @posts.route('/create', methods=['GET', 'POST'])
 def create_post_page():
     if request.method == 'POST':
@@ -66,5 +43,32 @@ def create_post_page():
 
     form = PostForm()
     return render_template('create_post_page.html', form=form)
+
+
+@posts.route('/')
+def index_page():
+    q = request.args.get('q')
+    if q:
+        psts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
+    else:
+        psts = Post.query.all()
+    return render_template('index.html', posts=psts)
+
+
+@posts.route('/<url>')
+def post_detail_page(url):
+    post = Post.query.filter(Post.url == url).first()
+    tags = post.tags
+    if post:
+        return render_template('post_detail.html', post=post, tags=tags)
+    else:
+        return render_template('page404.html'), 404
+
+
+@posts.route('/tag/<url>')
+def tag_detail_page(url):
+    tag = Tag.query.filter(Tag.url == url).first()
+    psts = tag.posts.all()
+    return render_template('tag_detail.html', tag=tag, psts=psts)
 
 
